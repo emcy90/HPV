98451179b01c04a149e8bdffb3118b3768db95bf
Emma Jarlvi Skog <emma@emcy.se>
2019-12-05 00:49:23 +0100

    Unit-test birthdate

--- HPVDataClasses/Person.py
+++ HPVDataClasses/Person.py
@@ -1,13 +1,52 @@
+import re
+import datetime
+
 class Person:
     """Class for handling personal data and test-results"""
-    def __init__(self, name: str='', personal_identification_number: str=''):
+    def __init__(self, name: str=None, birthdate: str=None, personal_identification_number: str=None):
         self.name = name
-        self.personal_identification_number = personal_identification_number.strip()
+        self.birthdate = birthdate
+        self.personal_identification_number = personal_identification_number
+        self.check_id()
+        self.check_date()
 
+    def check_id(self):
+        # Only validate id if id is given
+        if self.personal_identification_number:
+            # Clean up spaces
+            self.personal_identification_number = self.personal_identification_number.strip()
+
+            # Add century
+            if re.match(r'[0-9]{6}-[0-9]{4}', self.personal_identification_number):
+                # Match id but without century, add it
+                self.personal_identification_number = '19'+self.personal_identification_number
+            #if valid id parse date
+            if re.match(r'[0-9]{8}-[0-9]{4}', self.personal_identification_number):
+                parsed_date = datetime.datetime.strptime(self.personal_identification_number[:8], "%Y%m%d")
+                self.birthdate = parsed_date.strftime('%Y-%m-%d')
+
+    def check_date(self):
+        if self.birthdate:
+            # Convert string to datetime
+            err = None
+            try:
+                self.birthdate = datetime.date.fromisoformat(self.birthdate)
+            except ValueError as ex:
+                if hasattr(ex, 'message'):
+                    raise self.ValidationError("Birthdate %s is not valid (%s)" % (self.birthdate, ex.message))
+                else:
+                    raise self.ValidationError("Birthdate %s is not valid (%s)" % (self.birthdate, ex))
+
+                
     def validate(self) -> bool:
-        #self.personnummer
-        raise self.ValidationError("Random error")
+        # Only validate id if id is given
+        if self.personal_identification_number:
+            if not re.match(r'[0-9]{8}-[0-9]{4}', self.personal_identification_number):
+                raise self.ValidationError("Personal Identification Number: %s don't match  format ########-#### (19710321-1234)" % self.personal_identification_number)
         #raise Exception()
+        if not self.birthdate: 
+            raise self.ValidationError("No birthdate given")
+
         return True
 
     class ValidationError(Exception):
--- HPVDataClasses/unittest_Person.py
+++ HPVDataClasses/unittest_Person.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import date
 from Person import Person
 
 class Test_unittest_Person(unittest.TestCase):
@@ -10,23 +11,51 @@ class Test_unittest_Person(unittest.TestCase):
         a = Person()
         self.assertRaises(Person.ValidationError, a.validate)
 
-    def test_validation_personal_identification_number(self):
+    def test_validation_personal_identification_number_invalid(self):
         # An empty id is not valid
         self.assertRaises(Person.ValidationError,
-                          Person(personal_identification_number = "").validate)
+                          Person(personal_identification_number="").validate)
         # A negative negative number
         self.assertRaises(Person.ValidationError,
-                          Person(personal_identification_number = "-1").validate)
+                          Person(personal_identification_number="-1").validate)
         # Non valid numbers
         self.assertRaises(Person.ValidationError,
-                          Person(personal_identification_number = "654654").validate)
+                          Person(personal_identification_number="654654").validate)
         self.assertRaises(Person.ValidationError,
-                          Person(personal_identification_number = "654654654654654").validate)
+                          Person(personal_identification_number="654654654654654").validate)
+
+    def test_validation_personal_identification_number_valid(self):
         # Valid numbers
         try:
-            self.assertEqual(Person(personal_identification_number= "900101-4159").validate(), True)
+            self.assertEqual(Person(personal_identification_number="19910101-4159").validate(), True)
+            self.assertEqual(Person(personal_identification_number="900101-4159").validate(), True)
         except Person.ValidationError as ex:
             self.fail("Didn't except a ValidationError here\n%s" % ex.message)
+
+    def test_birthyear_valid(self):
+        try:
+            self.assertEqual(Person(birthdate='2003-12-04').validate(), True)
+            self.assertEqual(Person(birthdate='1910-11-30').validate(), True)
+            self.assertEqual(Person(birthdate='1980-12-04').validate(), True)
+            self.assertEqual(Person(birthdate='1991-06-30').validate(), True)
+        except Person.ValidationError as ex:
+            self.fail("Didn't except a ValidationError here\n%s" % ex.message)
+
+    def test_birthyear_invalid(self):
+        try:
+            Person(birthdate='1991-06-31').validate()
+        except Person.ValidationError as ex:
+            pass  # We except to get Validation error here, 31 is not a valid day for june
+        except Exception as ex:
+            self.fail("Didn't except exception (%s) here." % ex)
+        try:
+            Person(birthdate='19911131').validate()
+        except Person.ValidationError as ex:
+            pass  # We except to get Validation error here, no dashes
+        except Exception as ex:
+            self.fail("Didn't except exception (%s) here." % ex)
+
+
 
 if __name__ == '__main__':
     unittest.main()
